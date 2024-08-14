27a27023051e60c208d22f2d351582c4cd58c93a
a-levitt <artemis.levitt@gmail.com>
2024-08-14 16:26:10 +0200

    Delete post method implementation

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // GET All posts
 app.get("/posts", (req, res) => {
-  res.json(posts);
+    res.json(posts);
 });
 //GET a specific post by id
 app.get("/posts/:id", (req, res) => {
@@ -64,17 +64,24 @@ app.post("/posts", (req, res) => {
 });
 //PATCH a post when you just want to update one parameter
 app.patch("/posts/:id", (req, res) => {
-  const post = posts.find((p) => p.id === parseInt(req.params.id));
-  if (!post) return res.status(404).json({ message: "Post not found"});
+    const post = posts.find((p) => p.id === parseInt(req.params.id));
+    if (!post) return res.status(404).json({ message: "Post not found"});
 
-  if (req.body.title) post.title = req.body.title;
-  if (req.body.content) post.content = req.body.content;
-  if (req.body.author) post.author = req.body.author;
+    if (req.body.title) post.title = req.body.title;
+    if (req.body.content) post.content = req.body.content;
+    if (req.body.author) post.author = req.body.author;
 
-  res.json(post);
+    res.json(post);
 });
 //DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req, res) => {
+    const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
+    if (index === -1) return res.status(404).json({ message: "Post not found"});
+
+    posts.splice(index, 1);
+    res.json({ message: "Post deleted"});
+});
 
 app.listen(port, () => {
-  console.log(`API is running at http://localhost:${port}`);
+    console.log(`API is running at http://localhost:${port}`);
 });
